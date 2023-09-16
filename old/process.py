7dd62350dbe245e1e37c1c7af9917f25070f9d24
Kye <kye@apacmediasolutions.com>
2023-09-15 21:29:57 -0400

    clean up

--- old/process.py
+++ old/process.py
@@ -1,74 +1,56 @@
 import os
+import concurrent.futures
 from datasets import Dataset, load_from_disk
-import re
 
 class LucidrainsDataset:
-    """    
-    This class, LucidrainsDataset, has the following methods:
-
-    __init__(self, repos_dir): Initializes the class with the path to the lucidrains directory.
-
-    collect_python_files(self): Collects Python code snippets from all repositories in the lucidrains directory.
-
-    _collect_python_files_in_repo(self, repo_dir): Collects Python files from a single repository directory.
-
-    create_dataset(self): Creates a dataset from the collected Python code snippets.
-
-    ```
-    # Initialize the LucidrainsDataset with the path to the repositories directory
-    repos_dir = "lucidrains_repositories"
-    lucidrains_data = LucidrainsDataset(repos_dir)
-
-    # Collect Python code snippets and create a dataset
-    python_code_dataset = lucidrains_data.create_dataset()
-
-    # Save the dataset
-    python_code_dataset.save_to_disk("lucidrains_python_code_dataset")
-
-    # Load the saved dataset for further use
-    loaded_dataset = load_dataset("lucidrains_python_code_dataset")
-    ```
-
-    """
-    def __init__(self, repos_dir):
+    def __init__(self, repos_dir, max_file_size=5 * 1024 * 1024):  # 5 MB default max
         self.repos_dir = repos_dir
+        self.max_file_size = max_file_size
 
     def collect_python_files(self):
-        python_code_snippets = []
-        for repo_name in os.listdir(self.repos_dir):
-            repo_dir = os.path.join(self.repos_dir, repo_name)
-            if os.path.isdir(repo_dir):
-                python_files = self._collect_python_files_in_repo(repo_dir)
-                python_code_snippets.extend(python_files)
-        return python_code_snippets
+        all_snippets = []
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            for snippets in executor.map(
+                self._collect_python_files_in_repo, [os.path.join(self.repos_dir, repo_name) for repo_name in os.listdir(self.repos_dir)]
+            ):
+                all_snippets.extend(snippets)
+        return all_snippets
 
     def _collect_python_files_in_repo(self, repo_dir):
         python_files = []
         for root, _, files in os.walk(repo_dir):
             for file in files:
-                if file.endswith(".py") and file != "setup.py":
-                    with open(os.path.join(root, file), "r", encoding="utf-8") as f:
-                        python_code = f.read()
-                    python_files.append(python_code)
+                if file.endswith(".py"):
+                    file_path = os.path.join(root, file)
+                    if os.path.getsize(file_path) <= self.max_file_size:
+                        try:
+                            with open(file_path, "r", encoding="utf-8") as f:
+                                python_code = f.read()
+                            python_files.append({
+                                "python_code": python_code,
+                                "repo_name": os.path.basename(repo_dir),
+                                "file_path": os.path.relpath(file_path, repo_dir)
+                            })
+                        except UnicodeDecodeError:
+                            print(f"Failed to decode {file_path}")
         return python_files
 
     def create_dataset(self):
         python_code_snippets = self.collect_python_files()
-        dataset = Dataset.from_dict({"python_code": python_code_snippets})
+        dataset = Dataset.from_dict({
+            "python_code": [snippet["python_code"] for snippet in python_code_snippets],
+            "repo_name": [snippet["repo_name"] for snippet in python_code_snippets],
+            "file_path": [snippet["file_path"] for snippet in python_code_snippets]
+        })
         return dataset
 
-# Initialize the LucidrainsDataset with the path to the repositories directory
-repos_dir = "pytorch/pytorch-pytorch-ec8b58f"
+# Usage remains similar
+repos_dir = "pytorch_repositories"
 lucidrains_data = LucidrainsDataset(repos_dir)
 
-# Collect Python code snippets and create a dataset
 python_code_dataset = lucidrains_data.create_dataset()
 
-# Save the dataset
-python_code_dataset.save_to_disk("pytorch")
+python_code_dataset.save_to_disk("lucidrains_python_code_dataset")
 
-# Load the saved dataset for further use
-loaded_dataset = load_from_disk("pytorch")
-
-# Push the dataset to the Hugging Face Datasets Hub
-loaded_dataset.push_to_hub("kye/all-pytorch-code")
+loaded_dataset = load_from_disk("lucidrains_python_code_dataset")
+loaded_dataset.push_to_hub("kye/all-huggingface-python-code")
--- pytorch/downloader.py
+++ pytorch/downloader.py
@@ -1,79 +1,72 @@
 import os
 import requests
 import zipfile
+import time
 from zipfile import BadZipFile
 
 class GitHubRepoDownloader:
     """
     A utility class to download and unzip GitHub repositories.
-
-    Args:
-        username (str): GitHub username.
-        download_dir (str): Directory to store the downloaded repositories.
-
-    Attributes:
-        username (str): GitHub username.
-        api_url (str): API endpoint to fetch user's repositories.
-        download_dir (str): Directory to store the downloaded repositories.
-
-    Example:
-        downloader = GitHubRepoDownloader(username="lucidrains", download_dir="lucidrains_repositories")
-        downloader.download_repositories()
     """
-
-    def __init__(self, username, download_dir):
+    def __init__(self, username, download_dir, api_token=None, specific_repos=None):
         self.username = username
         self.api_url = f"https://api.github.com/users/{username}/repos"
         self.download_dir = download_dir
+        self.api_token = api_token
+        self.specific_repos = specific_repos if specific_repos else []
         os.makedirs(self.download_dir, exist_ok=True)
 
-    def download_repositories(self):
-        """
-        Downloads and unzips repositories from the user's GitHub account.
+    def _get_repositories(self):
+        headers = {}
+        if self.api_token:
+            headers["Authorization"] = f"token {self.api_token}"
 
-        Raises:
-            RuntimeError: If there is an error during download or unzip.
-        """
-        response = requests.get(self.api_url)
-        if response.status_code == 200:
-            repositories = response.json()
-            for repo in repositories:
-                repo_name = repo["name"]
-                repo_url = repo["html_url"]
-                zip_url = f"{repo_url}/archive/refs/heads/master.zip"
-                zip_file_path = os.path.join(self.download_dir, f"{repo_name}.zip")
-                zip_response = requests.get(zip_url)
-                if zip_response.status_code == 200:
-                    with open(zip_file_path, "wb") as zip_file:
-                        zip_file.write(zip_response.content)
-                    try:
-                        self._unzip_repository(zip_file_path)
-                        print(f"Downloaded and unzipped {repo_name}")
-                    except BadZipFile:
-                        print(f"Invalid ZIP file for {repo_name}")
-                else:
-                    print(f"Failed to download {repo_name}")
-        else:
-            raise RuntimeError(f"Failed to fetch repositories for user {self.username}")
-        print("All repositories downloaded and unzipped.")
+        all_repositories = []
+        page = 1
+        while True:
+            response = requests.get(f"{self.api_url}?page={page}", headers=headers)
+            if response.status_code != 200:
+                response.raise_for_status()
+            if not response.json():
+                break
+            all_repositories.extend(response.json())
+            page += 1
+            if "X-RateLimit-Remaining" in response.headers and int(response.headers["X-RateLimit-Remaining"]) == 0:
+                time.sleep(60)
+        return all_repositories
+
+    def download_repositories(self):
+        repositories = self._get_repositories()
+        for repo in repositories:
+            repo_name = repo["name"]
+            if self.specific_repos and repo_name not in self.specific_repos:
+                continue
+            repo_url = repo["html_url"]
+            zip_url = f"{repo_url}/archive/refs/heads/master.zip"
+            zip_file_path = os.path.join(self.download_dir, f"{repo_name}.zip")
+            zip_response = requests.get(zip_url)
+            if zip_response.status_code == 200:
+                with open(zip_file_path, "wb") as zip_file:
+                    zip_file.write(zip_response.content)
+                try:
+                    self._unzip_repository(zip_file_path)
+                    os.remove(zip_file_path)
+                    print(f"Downloaded and unzipped {repo_name}")
+                except BadZipFile:
+                    print(f"Invalid ZIP file for {repo_name}")
+            else:
+                print(f"Failed to download {repo_name}")
 
     def _unzip_repository(self, zip_file_path):
-        """
-        Unzips a repository ZIP file.
-
-        Args:
-            zip_file_path (str): Path to the ZIP file to unzip.
-
-        Raises:
-            BadZipFile: If the ZIP file is invalid.
-        """
         with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
             zip_ref.extractall(self.download_dir)
 
 # Example usage:
-downloader = GitHubRepoDownloader(username="lucidrains", download_dir="lucidrains_repositories")
-try:
-    downloader.download_repositories()
-except RuntimeError as e:
-    print(f"Error: {e}")
+downloader = GitHubRepoDownloader(
+    username="huggingface", 
+    download_dir="pytorch_repositories", 
+    api_token="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"
+)
+downloader.download_repositories()
+
 
